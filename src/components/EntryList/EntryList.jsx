bcb4e086f860defea8a9d9d9f65355139ba824de
Christian Beckham <urenacb22@gmail.com>
2022-10-17 15:37:26 -0500

    updated table HTML in EntryList component

--- src/components/EntryList/EntryList.jsx
+++ src/components/EntryList/EntryList.jsx
@@ -3,18 +3,20 @@ import React from "react";
 const EntryList = (props) => {
 	return (
 		<table>
-			<tr>
-				<th>Entry</th>
-				<th>Weight</th>
-				<th>Date</th>
-			</tr>
-			{props.entries.map((entry, index) => (
-				<tr key={index}>
-					<td>{index + 1}</td>
-					<td>{entry.weight}</td>
-					<td>{entry.date}</td>
+			<tbody>
+				<tr>
+					<th>Entry</th>
+					<th>Weight</th>
+					<th>Date</th>
 				</tr>
-			))}
+				{props.entries.map((entry, index) => (
+					<tr key={index}>
+						<td>{index + 1}</td>
+						<td>{entry.weight}</td>
+						<td>{entry.date}</td>
+					</tr>
+				))}
+			</tbody>
 		</table>
 	);
 };
